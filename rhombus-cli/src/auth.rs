2aea00e2bf3f7fe31ba7fe92d10a18f293316c78
Joshua Sims <jm8hjkl@gmail.com>
2025-01-13 16:20:40 -0500

    Secret and project config file

--- rhombus-cli/src/auth.rs
+++ rhombus-cli/src/auth.rs
@@ -1,9 +1,12 @@
+use std::{fs::create_dir_all, path::Path};
+
 use anyhow::{anyhow, Context, Result};
 
 use crate::{
-    get_client_from_config,
+    connect,
     grpc::proto::{whoami_reply::Whoami, WhoamiRequest},
-    Config,
+    read_project_config, read_secret_config, ProjectConfig, ProjectConfigYaml, SecretConfig,
+    SecretConfigYaml,
 };
 
 #[derive(clap::Parser, Debug)]
@@ -11,6 +14,9 @@ pub struct AuthCommand;
 
 impl AuthCommand {
     pub async fn run(&self) -> Result<()> {
+        let mut secret_config = read_secret_config()?;
+        let project_config = read_project_config().ok();
+
         let api_key = inquire::Password::new("API key?")
             .with_help_message("Get it from your account page")
             .with_display_mode(inquire::PasswordDisplayMode::Masked)
@@ -37,9 +43,17 @@ impl AuthCommand {
             None => inquire::prompt_text("CTF URL?")?,
         };
 
-        let config = Config { url, api_key };
+        if let Some(ref project_config) = project_config {
+            if &project_config.url != &url {
+                return Err(anyhow!(
+                    "API key url ('{}') does not match project config url ('{}')",
+                    &url,
+                    &project_config.url
+                ))?;
+            }
+        }
 
-        let mut client = get_client_from_config(&config).await?;
+        let mut client = connect(&url, &api_key).await?;
 
         let response = client.whoami(WhoamiRequest {}).await?;
         match response.into_inner().whoami {
@@ -52,10 +66,58 @@ impl AuthCommand {
             None => Err(anyhow!("invalid response from server"))?,
         }
 
-        std::fs::write("rhombus-cli.yaml", serde_yml::to_string(&config)?)
-            .context("failed to write rhombus-cli.yaml")?;
+        match project_config {
+            Some(project_config) => {
+                println!(
+                    "✓ Project config exists at {}",
+                    project_config.path.display()
+                );
+            }
+            None => {
+                let home_dir = directories::UserDirs::new().map(|p| p.home_dir().to_owned());
+                let default_project_config_dir = Path::new(".")
+                    .canonicalize()
+                    .ok()
+                    .and_then(|p| {
+                        p.ancestors()
+                            .find(|p| p.join(".git").is_dir())
+                            .map(|p| p.to_owned())
+                    })
+                    .and_then(|p| p.canonicalize().ok())
+                    .filter(|p| Some(p.as_path()) != home_dir.as_deref())
+                    .map(|s| s.as_os_str().to_string_lossy().to_string())
+                    .unwrap_or(".".to_owned());
 
-        println!("✓ Created rhombus-cli.yaml");
+                let project_config_dir = inquire::Text::new("Directory to write project config?")
+                    .with_initial_value(&default_project_config_dir)
+                    .prompt()?;
+
+                let project_config_path = Path::new(&project_config_dir).join("rhombus-cli.yaml");
+                std::fs::write(
+                    &project_config_path,
+                    serde_yml::to_string(&ProjectConfigYaml { url: url.clone() })?,
+                )
+                .with_context(|| format!("failed to write {}", project_config_path.display()))?;
+                println!("✓ Created project config {}", project_config_path.display());
+            }
+        }
+        secret_config.keys.insert(url.clone(), api_key);
+
+        let secret_config_path = secret_config.path.to_owned();
+        let secret_config_yaml: SecretConfigYaml = secret_config.into();
+        create_dir_all(secret_config_path.parent().unwrap()).with_context(|| {
+            format!(
+                "failed to create directory {}",
+                secret_config_path.parent().unwrap().display()
+            )
+        })?;
+        std::fs::write(
+            &secret_config_path,
+            serde_yml::to_string(&secret_config_yaml)?,
+        )
+        .with_context(|| format!("failed to write {}", secret_config_path.display()))?;
+
+        println!("✓ Wrote API key to {}", secret_config_path.display());
 
         Ok(())
     }
--- rhombus-cli/src/main.rs
+++ rhombus-cli/src/main.rs
@@ -8,7 +8,7 @@ use figment::{
 };
 use grpc::proto::rhombus_client::RhombusClient;
 use serde::{Deserialize, Serialize};
-use std::{path::PathBuf, process::exit};
+use std::{collections::BTreeMap, path::PathBuf, process::exit};
 use tonic::{
     metadata::MetadataValue,
     service::{interceptor::InterceptedService, Interceptor},
@@ -42,9 +42,47 @@ enum Command {
 }
 
 #[derive(Serialize, Deserialize)]
-struct Config {
+struct ProjectConfigYaml {
     url: String,
-    api_key: String,
+}
+
+struct ProjectConfig {
+    path: PathBuf,
+    url: String,
+}
+
+impl Into<ProjectConfigYaml> for ProjectConfig {
+    fn into(self) -> ProjectConfigYaml {
+        ProjectConfigYaml { url: self.url }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct SecretConfigYaml {
+    keys: Vec<UrlKeyPairYaml>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct UrlKeyPairYaml {
+    url: String,
+    key: String,
+}
+
+struct SecretConfig {
+    path: PathBuf,
+    keys: BTreeMap<String, String>,
+}
+
+impl Into<SecretConfigYaml> for SecretConfig {
+    fn into(self) -> SecretConfigYaml {
+        SecretConfigYaml {
+            keys: self
+                .keys
+                .into_iter()
+                .map(|(url, key)| UrlKeyPairYaml { url, key })
+                .collect(),
+        }
+    }
 }
 
 #[tokio::main]
@@ -104,32 +142,80 @@ type Client = RhombusClient<InterceptedService<Channel, AuthInterceptor>>;
 #[allow(dead_code)]
 /// Load the rhombus-cli.yaml config file and connect to the grpc server to which it refers
 async fn get_client() -> Result<Client> {
-    let config_file = find_config_file()?;
-    let config: Config = Figment::new()
-        .merge(Yaml::file_exact(&config_file))
-        .extract()
-        .with_context(|| format!("failed to load config file ({})", config_file.display()))?;
-    get_client_from_config(&config).await
+    let secret_config = read_secret_config()?;
+    let project_config = read_project_config()?;
+    let key = secret_config.keys.get(&project_config.url).ok_or_else(|| {
+        anyhow!(
+            "no key found for url {}. Run rhombus-cli auth",
+            &project_config.url
+        )
+    })?;
+    connect(&project_config.url, key).await
 }
 
-async fn get_client_from_config(config: &Config) -> Result<Client> {
-    let auth_token: MetadataValue<_> = config.api_key.parse()?;
-    let channel = Channel::from_shared(config.url.clone())
-        .with_context(|| format!("failed to parse grpc url '{}'", &config.url))?
+async fn connect(url: &str, key: &str) -> Result<Client> {
+    let auth_token: MetadataValue<_> = key.parse()?;
+    let channel = Channel::from_shared(url.to_owned())
+        .with_context(|| format!("failed to parse grpc url '{}'", url))?
         .connect()
         .await
-        .with_context(|| format!("failed to connect to grpc server '{}'", &config.url))?;
+        .with_context(|| format!("failed to connect to grpc server '{}'", url))?;
     let client = RhombusClient::with_interceptor(channel, AuthInterceptor { auth_token });
     Ok(client)
 }
 
-fn find_config_file() -> Result<PathBuf> {
-    for p in std::env::current_dir()?.ancestors() {
-        if p.join("rhombus-cli.yaml").is_file() {
-            return Ok(p.join("rhombus-cli.yaml").to_owned());
+fn find_project_config_file() -> Result<PathBuf> {
+    for path in std::env::current_dir()?.ancestors() {
+        if path.join("rhombus-cli.yaml").is_file() {
+            return Ok(path.join("rhombus-cli.yaml").to_owned());
         }
     }
     Err(anyhow!(
         "failed to find rhombus-cli.yaml. Run rhombus-cli auth"
     ))
+}
+
+fn read_project_config() -> Result<ProjectConfig> {
+    let path = find_project_config_file()?;
+    let config: ProjectConfigYaml = Figment::new()
+        .merge(Yaml::file_exact(&path))
+        .extract()
+        .with_context(|| format!("failed to load project config file ({})", path.display()))?;
+    Ok(ProjectConfig {
+        url: config.url,
+        path,
+    })
+}
+
+fn find_secret_config_file() -> Result<PathBuf> {
+    Ok(
+        directories::ProjectDirs::from("gg", "rhombus", "rhombus-cli")
+            .ok_or(anyhow!("failed to find config directory"))?
+            .config_dir()
+            .join("config.yaml")
+            .to_owned(),
+    )
+}
+
+fn read_secret_config() -> Result<SecretConfig> {
+    let path = find_secret_config_file()?;
+    if !path.exists() {
+        return Ok(SecretConfig {
+            path,
+            keys: BTreeMap::new(),
+        });
+    }
+    let config: SecretConfigYaml = Figment::new()
+        .merge(Yaml::file_exact(&path))
+        .extract()
+        .with_context(|| format!("failed to load config file ({})", path.display()))?;
+
+    Ok(SecretConfig {
+        keys: config
+            .keys
+            .into_iter()
+            .map(|pair| (pair.url, pair.key))
+            .collect(),
+        path,
+    })
 }
