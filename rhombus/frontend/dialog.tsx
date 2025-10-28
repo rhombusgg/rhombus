3d9d150ceffccdcf1dc1d239d598bce306a9e5b7
Joshua Sims <jm8hjkl@gmail.com>
2025-10-28 17:22:10 -0400

    fmt

--- rhombus/frontend/dialog.tsx
+++ rhombus/frontend/dialog.tsx
@@ -32,12 +32,12 @@ const DialogContent: ParentComponent<DialogPrimitive.DialogContentProps> = (
     <DialogPrimitive.Content
       {...props}
       class={cn(
-        "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border shadow-lg duration-200 sm:rounded-lg",
+        "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed top-[50%] left-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border shadow-lg duration-200 sm:rounded-lg",
         props.class,
       )}
     >
       {props.children}
-      <DialogPrimitive.CloseButton class="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+      <DialogPrimitive.CloseButton class="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-none disabled:pointer-events-none">
         <X class="h-4 w-4" />
         <span class="sr-only">Close</span>
       </DialogPrimitive.CloseButton>
@@ -73,7 +73,7 @@ const DialogTitle: ParentComponent<DialogPrimitive.DialogTitleProps> = (
 ) => (
   <DialogPrimitive.Title
     {...props}
-    class={cn("text-lg font-semibold leading-none tracking-tight", props.class)}
+    class={cn("text-lg leading-none font-semibold tracking-tight", props.class)}
   />
 );
 
